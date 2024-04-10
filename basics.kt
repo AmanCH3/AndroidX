183f71b1c37dfa7ff61c347e779a73108b91350c
AmanCH3 <armanachaudhary53@gmail.com>
2024-04-10 02:10:05 -0400

    Day 1 Basics

---
+++ basics.kt
@@ -0,0 +1,74 @@
+package com.example.andriodx
+//
+//fun main() {
+////    println("Hello world")
+////    var speed = 100;   //mutable variable
+////
+////    val speed = 100 ; // Immutable value
+////
+//      var age  = 10
+//    age  = 50
+//    println(age)
+//
+//
+//    val data = 2020
+////    data = 100
+//    println(data)
+//
+//
+////    var a : Boolean = true
+////    var b : Char = 'R'
+////    var c : Byte = 12
+////    var d : Short = -356
+////    var e : Int = 45
+////    var f : Long = -5324242L
+////    var i : Float = 5.6434F
+////    var h : Double = 7.32345
+////
+////    println(a)
+////    println(b)
+////    println(c)
+////    println(d)
+////    println(e)
+////    println(f)
+////    println(i)
+////    println(h)
+////
+//    var a : String  = "Hello world"
+//    var i : Int = a.length
+//    var x : Boolean = a.equals("Hello world")
+//    var username : String = "Softwarica"
+//
+//
+//    println(username.trim())
+//    println(a)
+//    println(i)
+//    println(a.isEmpty())
+//    println(a.lowercase())
+//    println(a.uppercase())
+//    println(x)
+//    print(a.plus(", How are you ?"))
+//
+//
+//
+//
+//}
+fun main (args: Arrray<String>){
+    var age = arrayOf(1,2,3)
+    println(age)
+    println("The first element od age is " + age[0])
+    println("The second elements of age is " + age[1])
+    println("The third elemets of  age is " + age[2])
+
+    println("***************************")
+
+
+    var name = arrayOf("ram" , "shyam" , "Hari")
+    name[1] = "Sandis"
+    println("The first elements of name is " + name[0])
+    println("The second elemets of name is " + name[1])
+    println("The third elemts od anme is " + name[2])
+
+
+    println(name.size)
+}
